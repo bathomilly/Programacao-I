7abd9c71447a1cea99b9a1541819a493e8d61b6c
Camilly Rodrigues Volk <414934@unoesc.lan>
2024-03-11 21:04:27 -0300

    Aula 04

--- Aula 04/Program.cs
+++ Aula 04/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//operadores
+int a = 3;
+int b = a++; // ++ é um acumulador
+//Neste caso ++ vai somar a + 1
+//Entretanto o acúmulo vai ocorrer depois da atribuição pois o ++ está do lado direito de a
+WriteLine($"a é {a}, b é {b}");
+//------------------------------
+
+int c = 3;
+int d = ++c;
+WriteLine($"c é {c}, d é {d}");
+//Neste caso o ++ está do lado esquerdo de c, portanto o acúmulo ocorre antes da atribuição
+//----------------------------------------------------------------------------------------
+
+//Combinando operadores de atribuição
+int p = 6;
+p += 3; //equivalente a p = p + 3;
+p -= 3; //equivalente a p = p -3;
+p *= 3; //equivalente a p = p * 3;
+p /= 3; //equivalente a p = p / 3;
+
+//OPERADORES LÓGICOS
+bool b_A = true;
+bool b_B = false;
+WriteLine($"AND  | b_A   | b_B  ");
+WriteLine($"b_A  | {b_A & b_A, -5} | {b_A & b_B, -5}");
+WriteLine($"b_B  | {b_B & b_A, -5} | {b_B & b_B, -5}");
+WriteLine();
+WriteLine($"OR   | b_A   | b_B  ");
+WriteLine($"b_A  | {b_A | b_A, -5} | {b_A | b_B, -5}");
+WriteLine($"b_B  | {b_B | b_A, -5} | {b_B | b_B, -5}");
+WriteLine();
+WriteLine($"XOR  | b_A   | b_B  ");
+WriteLine($"b_A  | {b_A ^ b_A, -5} | {b_A ^ b_B, -5}");
+WriteLine($"b_B  | {b_B ^ b_A, -5} | {b_B ^ b_B, -5}");
